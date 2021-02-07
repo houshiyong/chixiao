832bb641b1e8d90dc97743661e2d0b9a146d66fd
贾蒙 <761268171@qq.com>
2021-02-07 14:37:42 +0800

    测试上传

--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -53,7 +53,7 @@
     <div class="announcement">
       <van-notice-bar
         left-icon="volume-o"
-        text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。"
+        text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。qqqqqqqqqqqqqqqqqq"
       />
     </div>
   </div>
